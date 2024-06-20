0e7bcdaa60948f4eaa1e55ef76117752fbc2ccac
Mohannad Abu Ajameih <ajameih@yahoo.com>
2024-06-20 13:56:07 +0300

    Create countries.js

---
+++ countries.js
@@ -0,0 +1,15 @@
+fetch('countries.json')
+    .then(response => response.json())
+    .then(data => {
+        const select = document.getElementById('countrySelect');
+
+        data.forEach(country => {
+            const option = document.createElement('option');
+            option.value = country.code;
+            option.textContent = country.name;
+            select.appendChild(option);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching countries data:', error);
+    });
